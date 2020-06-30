54633bc5b6155fcefb0b8e312253db0d5b7e2518
hubernikus <hubernikus@gmail.com>
2020-06-30 15:15:11 +0200

    make angle and fix stability

--- scripts/example_ds_sinus.py
+++ scripts/example_ds_sinus.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
 
-def ds_sinus(pos, distance_x=6, sinus_magnitude=2,
-             flip_curve=False, move_backwards=False,
+def ds_sinus(pos, attractor_pos=[0, 1], sinus_magnitude=2,
+             flip_curve=False, move_to_origin=False,
              maximum_velocity=0.5, y_scaling=2):
     ''' 2D dynamical system which follows sinus wave form
 
@@ -11,17 +11,27 @@ def ds_sinus(pos, distance_x=6, sinus_magnitude=2,
     position: np.array of shape (2.)
     x_position: float '''
 
-    desired_y = np.sin(pos[0]/distance_x*pi) * sinus_magnitude
+    angle_attractor = np.arctan2(attractor_pos[1], attractor_pos[0])
+    distance_attractor = np.linalg.norm(attractor_pos)
+
+    cos_ang = np.cos(angle_attractor)
+    sin_ang = np.sin(angle_attractor)
+    rot_matrix = np.array([[cos_ang, sin_ang],
+                           [-sin_ang, cos_ang]])
+
+    pos = rot_matrix.dot(pos)
+
+    desired_y = np.sin(pos[0]/distance_attractor*pi) * sinus_magnitude
     if flip_curve:
         desired_y *= (-1)
 
     vel = np.zeros(2)
     vel[1] = desired_y-pos[1]
         
-    vel[0] = distance_x-pos[0]
-    if move_backwards:
-        vel[0] *= (-1)
-
+    if move_to_origin:
+        vel[0] = -pos[0]
+    else:
+        vel[0] = distance_attractor-pos[0]
 
     if  np.abs(vel[0]) > maximum_velocity:
         vel[0] = np.copysign(maximum_velocity, vel[0])
@@ -33,14 +43,16 @@ def ds_sinus(pos, distance_x=6, sinus_magnitude=2,
     norm_vel = np.linalg.norm(vel)
     if norm_vel > maximum_velocity:
         vel = vel/norm_vel * maximum_velocity
-
+        
+    vel = rot_matrix.T.dot(vel)
+    
     return vel
     
 dim = 2 # space dimension    
-x_range = [-1, 7]
-y_range = [-3, 3]
+x_range = [-7, 7]
+y_range = [-7, 7]
 
-attractor_pos = np.array([6, 0])
+attractor_pos = np.array([3, 3])
 
 n_grid = 20
 
@@ -55,9 +67,9 @@ vel = np.zeros((n_grid, n_grid, dim))
 
 for ix in range(n_grid):
     for iy in range(n_grid):
-        vel[ix, iy, :] = ds_sinus(pos[ix, iy, :], 6,
+        vel[ix, iy, :] = ds_sinus(pos[ix, iy, :], attractor_pos,
                                   flip_curve=True,
-                                  move_backwards=True)
+                                  move_to_origin=True)
 # Draw plot
 plt.ion()
 # fig, ax = plt.figure()
